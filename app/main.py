698edfb51cee26be926d02acb886a9caf2fe8d93
joaovictormnzsl <jvictormenezesl@gmail.com>
2026-03-13 01:51:57 -0300

    removendo o tasks.db do controle do git

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.database import Base, engine
-from app.routes import task_routes
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-
-app.include_router(task_routes.router)
